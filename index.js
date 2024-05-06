e95740cbdeb9449338d47855a6e179817b3e55ea
Lalit-threely <147034093+Lalit-threely@users.noreply.github.com>
2024-05-06 23:11:30 +0530

    bug fixes

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ mongoose
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.setHeader('Access-Control-Allow-Origin', "*");
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
