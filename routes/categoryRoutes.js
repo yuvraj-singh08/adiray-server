e52197a729609cf8ba1d1e1b0982685f9a22793c
yuvraj-singh08 <yuvrajsinghwahla08cs@gmail.com>
2024-05-15 17:25:53 +0530

    Final

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -5,14 +5,14 @@ const adminAuth = require('../middleware/adminAuth.js');
 
 const router = Router();
 
-router.post('/create', createCategory);
+router.post('/create', adminAuth, createCategory);
 // router.post('/subcategory/create', createSubCategory);
-router.post('/product/create', addProduct);
-router.get('/', adminAuth , getCategoryList);
+router.post('/product/create', adminAuth, addProduct);
+router.get('/', adminAuth, getCategoryList);
 // router.get('/subcategory/:categoryId', getSubCategoryList);
-router.get('/product/:categoryId/', getCategoryData);
-router.post('/update', updateCategory);
-router.post('/product/update', updateProduct);
+router.get('/product/:categoryId/', adminAuth, getCategoryData);
+router.post('/update', adminAuth, updateCategory);
+router.post('/product/update', adminAuth, updateProduct);
 
 module.exports = router;
 
